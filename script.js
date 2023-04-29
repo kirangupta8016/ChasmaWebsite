37d91408df0151662eb93acd281b0ae8fc1cf461
kiran <kirangupta8016@gmail.com>
2023-04-29 19:26:20 +0530

    chasma website

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    // margin:2,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
